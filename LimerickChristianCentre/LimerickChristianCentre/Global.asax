4a0a1e2cac620beb40f9db41a24265ee3009850a
profesor <git@bernas.it>
2016-02-21 22:57:38 +0000

    Downgraded project to net 4.5 for hosting purposes

---
+++ LimerickChristianCentre/LimerickChristianCentre/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LimerickChristianCentre.MvcApplication" Language="C#" %>
---
+++ LimerickChristianCentre/LimerickChristianCentre/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LimerickChristianCentre.Startup))]
+namespace LimerickChristianCentre
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
