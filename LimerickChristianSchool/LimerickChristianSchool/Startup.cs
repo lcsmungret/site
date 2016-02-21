17eb553d4eaa4aa7f8c48a18bbd9fa018b8d8af8
profesor <git@bernas.it>
2016-02-21 21:26:37 +0000

    added blank project
    d

---
+++ LimerickChristianSchool/LimerickChristianSchool/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LimerickChristianSchool.Startup))]
+namespace LimerickChristianSchool
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
